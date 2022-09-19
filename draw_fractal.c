56dad3d6df625bdd794b566c82cf65e790740b55
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-09-19 13:48:48 -0300

    Add files via upload

--- draw_fractal.c
+++ draw_fractal.c
@@ -6,49 +6,60 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 10:11:07 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/09/19 12:30:51 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/09/19 13:48:14 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	draw_fractal(t_fractol *mlx, int arg)
+void	draw_julia(t_fractol *mlx)
 {
 	int		x;
 	int		y;
 	double	num_real;
 	double	num_imaginary;
 
+	y = -1;
+	while (++y < HEIGHT)
+	{
+		x = -1;
+		while (++x < WIDTH)
+		{
+			num_real = mlx->min_r + (double)x
+				* (mlx->max_r - mlx->min_r) / WIDTH;
+			num_imaginary = mlx->min_i + (double)y
+				* (mlx->max_i - mlx->min_i) / HEIGHT;
+			julia(mlx, x, y, num_real, num_imaginary);
+		}
+	}
+}
+
+void	draw_mandelbrot(t_fractol *mlx)
+{
+	int		x;
+	int		y;
+	double	num_real;
+	double	num_imaginary;
+
+	y = -1;
+	while (++y < HEIGHT)
+	{
+		x = -1;
+		while (++x < WIDTH)
+		{
+			num_real = mlx->min_r + (double)x
+				* (mlx->max_r - mlx->min_r) / WIDTH;
+			num_imaginary = mlx->min_i + (double)y
+				* (mlx->max_i - mlx->min_i) / HEIGHT;
+			mandelbrot(mlx, x, y, num_real, num_imaginary);
+		}
+	}
+}
+
+void	draw_fractal(t_fractol *mlx, int arg)
+{
 	if (arg > 1)
-	{
-		y = -1;
-		while (++y < HEIGHT)
-		{
-			x = -1;
-			while (++x < WIDTH)
-			{
-				num_real = mlx->min_r + (double)x
-					* (mlx->max_r - mlx->min_r) / WIDTH;
-				num_imaginary = mlx->min_i + (double)y
-					* (mlx->max_i - mlx->min_i) / HEIGHT;
-				julia(mlx, x, y, num_real, num_imaginary);
-			}
-		}
-	}
+		draw_julia(&mlx);
 	else
-	{
-		y = -1;
-		while (++y < HEIGHT)
-		{
-			x = -1;
-			while (++x < WIDTH)
-			{
-				num_real = mlx->min_r + (double)x
-					* (mlx->max_r - mlx->min_r) / WIDTH;
-				num_imaginary = mlx->min_i + (double)y
-					* (mlx->max_i - mlx->min_i) / HEIGHT;
-				mandelbrot(mlx, x, y, num_real, num_imaginary);
-			}
-		}
-	}
+		draw_mandelbrot(&mlx);
 }
