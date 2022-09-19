72e786e45e233af0ddd82ad17bcd437209fee566
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-09-19 13:35:19 -0300

    Add files via upload

--- fractol.c
+++ fractol.c
@@ -6,11 +6,19 @@
 /*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 11:31:06 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/09/19 13:29:22 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/09/19 13:31:13 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+void	error(void)
+{
+	write(1, "invalid number of arguments!", 28);
+	write(1, "./fractol julia <real number> <imaginary number>", 48);
+	write(1, "\n", 1);
+	write(1, "./fractol mandelbrot", 20);
+}
 
 int	main(int argc, char **argv)
 {
@@ -20,10 +28,7 @@ int	main(int argc, char **argv)
 
 	if (argc == 1 || argc > 4)
 	{
-		write(1, "invalid number of arguments!", 28);
-		write(1, "./fractol julia <real number> <imaginary number>", 48);
-		write(1, "\n", 1);
-		write(1, "./fractol mandelbrot", 20);
+		error();
 		return (0);
 	}
 	else if (argc > 2 && argc < 5)
