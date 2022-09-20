cd356a6c8b88f6f5d64d4ae75b373a734fa0e641
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-09-19 21:18:47 -0300

    Update fractol.c

--- fractol.c
+++ fractol.c
@@ -12,6 +12,57 @@
 
 #include "fractol.h"
 
+int count_power(const char *str)
+{
+    int i;
+    int post_comma;
+
+    i = 0;
+    post_comma = 0;
+    while (str[i])
+    {
+        if (str[i] == '.')
+        {
+            while(str[i])
+            {
+                i++;
+                post_comma++;
+            }
+        }
+        i++;
+    }
+    return (pow(10, post_comma - 1));
+}
+
+double	ft_atod(const char *str)
+{
+	int     i;
+	int     signal;
+	double  result;
+	int     power;
+
+	result = 0;
+	signal = 1;
+	i = 0;
+	if (str[i] == '-')
+		signal = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] >= 48 && str[i] <= 57 || str[i] == '.')
+	{
+		if (result >= 9223372036854775807 && signal == 1)
+			return (-1);
+		if (result > 9223372036854775807 && signal == -1)
+			return (0);
+        if (str[i] == '.')
+            i++;
+		result = (str[i] - 48) + (result * 10);
+		i++;
+	}
+	power = count_power(str);
+	return ((result * signal) / power);
+}
+
 void	error(void)
 {
 	write(1, "invalid number of arguments!", 28);
@@ -33,6 +84,8 @@ int	main(int argc, char **argv)
 	}
 	else if (argc > 2 && argc < 5)
 	{
+		kr = ft_atoid(argv[2]);
+		ki = ft_atoid(argv[3]);
 		mlx.kr = kr;
 		mlx.ki = ki;
 	}
