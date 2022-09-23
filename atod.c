83123eac7c876cd97c183e949598da600cbc8f51
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-09-23 13:56:57 -0300

    Add files via upload

--- atod.c
+++ atod.c
@@ -1,52 +1,98 @@
-#include <math.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   atod.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpimenta <mpimenta@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 09:23:50 by mpimenta          #+#    #+#             */
+/*   Updated: 2022/09/23 13:08:59 by mpimenta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int count_power(const char *str)
+#include "fractol.h"
+
+int	ft_atoi(const char *str)
 {
-    int i;
-    int post_comma;
-
-    i = 0;
-    post_comma = 0;
-    while (str[i])
-    {
-        if (str[i] == '.')
-        {
-            while(str[i])
-            {
-                i++;
-                post_comma++;
-            }
-        }
-        i++;
-    }
-    return (pow(10, post_comma - 1));
-}
-
-double	ft_atod(const char *str)
-{
-	int     i;
-	int     signal;
-	double  result;
-    int     power;
+	int					i;
+	int					signal;
+	unsigned long int	result;
 
 	result = 0;
 	signal = 1;
 	i = 0;
+	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t'
+		|| str[i] == '\r' || str[i] == '\v' || str[i] == '\f')
+		i++;
 	if (str[i] == '-')
 		signal = -1;
 	if (str[i] == '-' || str[i] == '+')
 		i++;
-	while ((str[i] >= 48 && str[i] <= 57) || str[i] == '.')
+	while (str[i] >= 48 && str[i] <= 57)
 	{
 		if (result >= 9223372036854775807 && signal == 1)
 			return (-1);
 		if (result > 9223372036854775807 && signal == -1)
 			return (0);
-        if (str[i] == '.')
-            i++;
 		result = (str[i] - 48) + (result * 10);
 		i++;
 	}
-    power = count_power(str);
-	return ((result * signal) / power);
+	return (result * signal);
+}
+
+size_t	nbsize_atof(double n)
+{
+	size_t	i;
+	int		x;
+
+	x = (int)n;
+	i = 0;
+	if (x <= 0)
+		i++;
+	while (x != 0)
+	{
+		i++;
+		x = x / 10;
+	}
+	return (i);
+}
+
+double	check_signal(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		return (-1);
+	else
+		return (1);
+}
+
+double	ft_atod(char *str)
+{
+	double	number;
+	size_t	size;
+	int		i;
+	double	signal;
+	double	p;
+
+	i = 1;
+	p = 1;
+	signal = check_signal(str);
+	number = (double)ft_atoi((const char *)str);
+	if (signal < 0)
+	{
+		number = -number;
+		i = 2;
+	}
+	size = nbsize_atof(number);
+	while (str[size + i] >= '0' && str[size + i] <= '9')
+	{
+		number += (str[size + i] - 48) / pow(10, p);
+		i++;
+		p++;
+	}
+	return (number * signal);
 }
