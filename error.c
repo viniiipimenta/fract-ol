fb9cd8c7a9881e138b716b1e30196e9badfc4346
Marcus Vinicius Pimenta <57456066+vinniipimenta@users.noreply.github.com>
2022-09-22 21:46:24 -0300

    Add files via upload

--- error.c
+++ error.c
@@ -6,36 +6,15 @@
 /*   By: mpimenta <mpimenta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 10:37:10 by mpimenta          #+#    #+#             */
-/*   Updated: 2022/09/22 21:34:38 by mpimenta         ###   ########.fr       */
+/*   Updated: 2022/09/22 21:46:06 by mpimenta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	error_name(void)
-{
-	write(1, "Wrong name!", 11);
-	write(1, "\n", 1);
-	write(1, "./fractol julia <num between -2.0 ", 34); 
-	write(1, "and 2.0> <num between -2.0 and 2.0>", 35);
-	write(1, "\n", 1);
-	write(1, "./fractol mandelbrot", 20);
-	write(1, "\n", 1);
-}
-
-
-void	error_size(void)
-{
-	write(1, "Wrong size!", 11);
-	write(1, "\n", 1);
-	write(1, "./fractol julia <num between -2.0 ", 34); 
-	write(1, "and 2.0> <num between -2.0 and 2.0>", 35);
-	write(1, "\n", 1);
-}
-
 void	error(void)
 {
-	write(1, "invalid number of arguments!", 28);
+	write(1, "Try one of these options", 24);
 	write(1, "\n", 1);
 	write(1, "./fractol julia <num between -2.0", 33); 
 	write(1, "and 2.0> <num between -2.0 and 2.0>", 35);
@@ -56,7 +35,7 @@ size_t	ft_strlen(const char *s)
 	return (count);
 }
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t			i;
 	unsigned char	*str;
@@ -65,12 +44,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	str = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	if (n == 0)
-		return (0);
 	if (ft_strlen(s1) == 0 || ft_strlen(s2) == 0)
 		return (str[i] - str2[i]);
-	while ((i < n - 1)
-		&& (str[i] == str2[i]) && (str[i] != '\0' || str2[i] != '\0'))
+	while (str[i] == str2[i] && (str[i] != '\0' || str2[i] != '\0'))
 		i++;
 	return (str[i] - str2[i]);
 }
